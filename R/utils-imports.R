8ef1ae9671e3667cde2b1c8e372bf16cc6c610b7
akselthomsen <thomsenaksel@gmail.com>
2020-03-20 14:22:31 +0100

    better naming -> all imports in one

--- R/utils-imports.R
+++ R/utils-imports.R
@@ -1,3 +1,16 @@
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+
 #' Collect
 #'
 #' See \code{dplyr::\link[dplyr:collect]{collect}} for details.
